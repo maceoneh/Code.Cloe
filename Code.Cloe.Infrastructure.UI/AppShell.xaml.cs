1d931adfc2e7e343a43cdc6bbc472d0be00860f2
maceoneh <davidmorenomagin@gmail.com>
2024-05-17 16:36:14 +0200

    .

--- Code.Cloe.Infrastructure.UI/AppShell.xaml.cs
+++ Code.Cloe.Infrastructure.UI/AppShell.xaml.cs
@@ -23,8 +23,8 @@ namespace Code.Cloe.Infrastructure.UI
             //----- Se guarda la ruta por si las clases Factory las necesitan para instanciar
             Factories.Services.Create.RepositoryFolder = localDataFolder;
             //----- Se genera la estructura de la base de datos
-            var repository = new RepositoryContext(localDataFolder);
-            repository.Migrate();
+            //var repository = new RepositoryContext(localDataFolder);
+            //repository.Migrate();
             
         }
     }
